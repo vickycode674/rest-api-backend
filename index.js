27eafffda018940bc8c91728eababb2e93c70a49
Chengalapattu Vivek Sai <chvivek674@gmail.com>
2024-08-02 19:28:49 +0530

    Update index.js and routes

--- index.js
+++ index.js
@@ -9,16 +9,6 @@ app.use(bodyParser.json());
 // Routes
 app.use('/api', dataRoutes);
 
-// Error handling middleware
-app.use((req, res, next) => {
-  res.status(404).send('Not Found');
-});
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { processData, getOperationCode } = require('../controllers/dataController');
 
-// Define routes
 router.post('/data', processData);
 router.get('/data', getOperationCode);
 
