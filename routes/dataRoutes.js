9a054c6ea5766e9e6175750404ee7a986c453e20
Chengalapattu Vivek Sai <chvivek674@gmail.com>
2024-08-02 20:20:22 +0530

    first-commit

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const dataController = require('./controllers/dataController');
+const { processData, getOperationCode } = require('../controllers/dataController');
 
-router.get('/somepath', dataController.getData); // Ensure this is correct
+router.post('/data', processData);
+router.get('/data', getOperationCode);
+
+module.exports = router;
